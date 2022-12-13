d5c06a43201a7cee787e384c0c8da86862145c94
Sergey Plotnikov <36457305+Grey1900@users.noreply.github.com>
2022-12-13 12:02:01 +0400

    modify cpp file

---
+++ task_11_3.cpp
@@ -0,0 +1,44 @@
+/******************************************************************************/
+/* File Name: Task_11_3                                                       */
+/* Data: 22.11.2022                                                           */
+/* Developer: Sergey Plotnikov                                                */
+/* Description: Solution for task 3 of Block 11 of the C++ study course       */
+/******************************************************************************/
+
+#include <iostream>
+#include <string>
+
+int octetCheck(std::string, int);
+
+int main()
+{
+    std::string ipAdress;
+
+    std::cout << "Enter IP-adress: ";
+    std::cin >> ipAdress;
+
+    for(int i = 0; i < 3; i++){
+        int octet = i;
+        if (octetCheck(std::string ipAdress, int octet) != 0) {
+            std::cout << "Invalid";
+            break;
+        } else {
+            std::cout << "Valid";
+        }
+    }
+
+    return 0;
+}
+
+int octetCheck(std::string ipAdress, int octet){
+    int error = 0;
+
+
+
+    return error;
+}
+
+std::string selectOctet(std::string ipAdress, int octet){
+    char const sep = '.';
+    std::string partIp = ipAdress.substr(octet, email.find(sep))
+}
